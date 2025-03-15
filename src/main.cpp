f57fe1466ed9bc648b8d004690af8a9ca31e9b04
Lucy Park <lucypark@outlook.com>
2025-03-14 18:13:00 -0600

    added files for sunnys comp to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 #include <Arduino.h>
+#define right_encoder_button 0
+#define right_encoder_channelA 1
+#define right_encoder_channelB 2
+
+int right_encoder_aLastState = 0;
 
 // put function declarations here:
 int myFunction(int, int);
 
+
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //Setting up right encoder
+  pinMode(right_encoder_button, INPUT);
+  pinMode(right_encoder_channelA, INPUT);
+  pinMode(right_encoder_channelB, INPUT);
+
+  int right_encoder_aLastState = 0;
+
+  Serial.begin (9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  right_encoder_aLastState = digitalRead(right_encoder_channelA);   
+  Serial.println(right_encoder_aLastState);
+
 }
 
 // put function definitions here:
